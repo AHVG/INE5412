041bbb99377702bc552fb718ece6978926b420ed
Micael <mikal2003.sp@gmail.com>
2023-09-03 18:30:38 -0300

    transformando em classe

---
+++ Atividade_1/src/scheduling_algorithms.h
@@ -0,0 +1,34 @@
+#ifndef _H_SCHEDULING_ALGORITHMS
+#define _H_SCHEDULING_ALGORITHMS
+
+
+
+#include <vector>
+#include "process.h"
+#include "algorithm"
+class SchedulingAlgorithms{
+public:
+    SchedulingAlgorithms(){};
+    ~SchedulingAlgorithms(){};
+    std::vector<Process *> FCFS(std::vector<Process *> processes){
+        return processes;
+    }
+    std::vector<Process *> SJF(std::vector<Process *> processes){
+        std::sort(processes.begin(), processes.end(), compareByDuration);
+        return processes;
+    }
+    std::vector<Process *> priority(std::vector<Process *> processes){
+        std::sort(processes.begin(), processes.end(), compareByPriority);
+        return processes;
+    }
+
+    bool compareByDuration(const Process& a, const Process& b) {
+        return a.getDuration() < b.getDuration();
+    }
+
+    bool compareByPriority(const Process& a, const Process& b) {
+        return a.getPriority() > b.getPriority();
+    }
+};
+
+#endif
