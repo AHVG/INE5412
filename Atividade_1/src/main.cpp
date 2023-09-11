516f06b75f0753b95a0017ac693aa10a6fc43fa9
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-09-11 07:39:51 -0300

    Pega

--- Atividade_1/src/main.cpp
+++ Atividade_1/src/main.cpp
@@ -10,7 +10,7 @@
 
 int main() {
 
-    std::cout << "\033[1;31mSimulador de algoritmos de escalonamento\033[0m\n\n";
+    customCout("Simulador de algoritmos de escalonamen", BRIGHT_RED);
 
     std::map<std::string, SchedulingAlgorithm *> algorithms = { {"FCFS", new FCFS()},
                                                                 {"SJF", new SJF()},
@@ -24,36 +24,6 @@ int main() {
         customCout("FIM DA SIMULACAO DO " + algorithm.first + "\n\n", BRIGHT_YELLOW);
     }
     for (auto &algorithm : algorithms) delete algorithm.second;
-
-    customCout("P", BLACK);
-    customCout("A", RED);
-    customCout("L", GREEN);
-    customCout("E", YELLOW);
-    customCout("T", BLUE);
-    customCout("A", MAGENTA);
-    customCout(" ", CYAN);
-    customCout("D", CYAN);
-    customCout("E", WHITE);
-    customCout(" ", BRIGHT_BLACK);
-    customCout("C", BRIGHT_BLACK);
-    customCout("O", BRIGHT_RED);
-    customCout("R\n", BRIGHT_GREEN);
-    customCout("Texto em Preto\n", BLACK);
-    customCout("Texto em Vermelho\n", RED);
-    customCout("Texto em Verde\n", GREEN);
-    customCout("Texto em Amarelo\n", YELLOW);
-    customCout("Texto em Azul\n", BLUE);
-    customCout("Texto em Magenta\n", MAGENTA);
-    customCout("Texto em Ciano\n", CYAN);
-    customCout("Texto em Branco\n", WHITE);
-    customCout("Texto em Preto Brilhante\n", BRIGHT_BLACK);
-    customCout("Texto em Vermelho Brilhante\n", BRIGHT_RED);
-    customCout("Texto em Verde Brilhante\n", BRIGHT_GREEN);
-    customCout("Texto em Amarelo Brilhante\n", BRIGHT_YELLOW);
-    customCout("Texto em Azul Brilhante\n", BRIGHT_BLUE);
-    customCout("Texto em Magenta Brilhante\n", BRIGHT_MAGENTA);
-    customCout("Texto em Ciano Brilhante\n", BRIGHT_CYAN);
-    customCout("Texto em Branco Brilhante\n", BRIGHT_WHITE);
 
     return 0;
 }
--- Atividade_1/src/utils.h
+++ Atividade_1/src/utils.h
@@ -20,6 +20,22 @@
 #define BRIGHT_MAGENTA "\033[1;35m"
 #define BRIGHT_CYAN "\033[1;36m"
 #define BRIGHT_WHITE "\033[1;37m"
+#define BLACK_BACKGROUND "\033[1;40m"
+#define RED_BACKGROUND "\033[1;41m"
+#define GREEN_BACKGROUND "\033[1;42m"
+#define YELLOW_BACKGROUND "\033[1;43m"
+#define BLUE_BACKGROUND "\033[1;44m"
+#define MAGENTA_BACKGROUND "\033[1;45m"
+#define CYAN_BACKGROUND "\033[1;46m"
+#define WHITE_BACKGROUND "\033[1;47m"
+#define WEAK_BLACK_BACKGROUND "\033[1;100m"
+#define WEAK_RED_BACKGROUND "\033[1;101m"
+#define WEAK_GREEN_BACKGROUND "\033[1;102m"
+#define WEAK_YELLOW_BACKGROUND "\033[1;103m"
+#define WEAK_BLUE_BACKGROUND "\033[1;104m"
+#define WEAK_MAGENTA_BACKGROUND "\033[1;105m"
+#define WEAK_CYAN_BACKGROUND "\033[1;106m"
+#define WEAK_WHITE_BACKGROUND "\033[1;107m"
 
 void customCout(std::string text, std::string color);
 void setColor(std::string color);
