f2a48299969855d771f348b76d9404e941e445a8
Micael <mikal2003.sp@gmail.com>
2023-09-12 15:04:33 -0300

    fazendo interface pra selecao de algoritmos

--- Atividade_1/src/main.cpp
+++ Atividade_1/src/main.cpp
@@ -10,20 +10,49 @@
 int main() {
 
     customCout("SIMULADOR DE ALGORITMO DE ESCALONAMENTO\n\n", BRIGHT_CYAN);
-
-    std::vector<std::pair<std::string, SchedulingAlgorithm *>> algorithms = { {"FCFS", new FCFS()},
-                                                                {"SJF", new SJF()},
-                                                                {"PRIORITY", new Priority()},
-                                                                {"PRIORITY WITH PREEMPTION", new PriorityWithPreemption()},
-                                                                {"ROUND ROBIN", new RoundRobin()}};
-    for (auto algorithm : algorithms) {
-        customCout("SIMULANDO " + algorithm.first + "\n\n", BRIGHT_YELLOW);
-        Kernel kernel(algorithm.second);
-        kernel.run();
-        // Será que é viavel fazer a análise do kernel após ele executar, colocando o analyzer aqui?
-        customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
+    while(1){
+        customCout("INSIRA O NOME DO ALGORITMO DE ESCALONAMENTO QUE DESEJA SIMULAR\n", BRIGHT_CYAN);
+        customCout("OPCOES: 1 -> FCFS, 2 -> SJF, 3 -> PRIORITY, 4 -> PRIORITY WITH PREEMPTION,  5-> ROUND ROBIN, 6 -> SAIR\n", BRIGHT_CYAN);
+        int option;
+        std::cin >> option;
+        std::cout << std::endl;
+        if (option == 1){
+            customCout("SIMULANDO FCFS\n\n", BRIGHT_YELLOW);
+            Kernel kernel(new FCFS());
+            kernel.run();
+            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
+        }
+        else if (option == 2){
+            customCout("SIMULANDO SJF\n\n", BRIGHT_YELLOW);
+            Kernel kernel(new SJF());
+            kernel.run();
+            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
+        }
+        else if (option == 3){
+            customCout("SIMULANDO PRIORITY\n\n", BRIGHT_YELLOW);
+            Kernel kernel(new Priority());
+            kernel.run();
+            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
+        }
+        else if (option == 4){
+            customCout("SIMULANDO PRIORITY WITH PREEMPTION\n\n", BRIGHT_YELLOW);
+            Kernel kernel(new PriorityWithPreemption());
+            kernel.run();
+            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
+        }
+        else if (option == 5){
+            customCout("SIMULANDO ROUND ROBIN\n\n", BRIGHT_YELLOW);
+            Kernel kernel(new RoundRobin());
+            kernel.run();
+            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
+        }
+        else if (option == 6){
+            break;
+        }
+        else{
+            customCout("OPCAO INVALIDA\n\n", BRIGHT_RED);
+        }
     }
-    for (auto &algorithm : algorithms) delete algorithm.second;
 
     customCout("FIM DAS SIMULACOES\n\n", BRIGHT_CYAN);
 
