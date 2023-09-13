4e3086c30c7a5212c0b246f9e3782e60fcca85e3
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-09-12 22:03:58 -0300

    Simplificação do código da interface e começando a inserir mais entradas

--- Atividade_1/src/main.cpp
+++ Atividade_1/src/main.cpp
@@ -10,68 +10,39 @@
 int main() {
 
     customCout("SIMULADOR DE ALGORITMO DE ESCALONAMENTO\n\n", BRIGHT_CYAN);
-    while(1){
-        customCout("INSIRA O NOME DO ALGORITMO DE ESCALONAMENTO QUE DESEJA SIMULAR\n", BRIGHT_CYAN);
-        customCout("OPCOES: 1 -> FCFS, 2 -> SJF, 3 -> PRIORITY, 4 -> PRIORITY WITH PREEMPTION,  5-> ROUND ROBIN, 6 -> RODAR TODOS, 7 -> SAIR\n", BRIGHT_CYAN);
-        int option;
-        std::cin >> option;
-        std::cout << std::endl;
-        if (option == 1){
-            customCout("SIMULANDO FCFS\n\n", BRIGHT_YELLOW);
-            Kernel kernel(new FCFS());
-            kernel.run();
-            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
-        }
-        else if (option == 2){
-            customCout("SIMULANDO SJF\n\n", BRIGHT_YELLOW);
-            Kernel kernel(new SJF());
-            kernel.run();
-            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
-        }
-        else if (option == 3){
-            customCout("SIMULANDO PRIORITY\n\n", BRIGHT_YELLOW);
-            Kernel kernel(new Priority());
-            kernel.run();
-            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
-        }
-        else if (option == 4){
-            customCout("SIMULANDO PRIORITY WITH PREEMPTION\n\n", BRIGHT_YELLOW);
-            Kernel kernel(new PriorityWithPreemption());
-            kernel.run();
-            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
-        }
-        else if (option == 5){
-            customCout("SIMULANDO ROUND ROBIN\n\n", BRIGHT_YELLOW);
-            Kernel kernel(new RoundRobin());
-            kernel.run();
-            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
-        }
-        else if(option == 6){
-            std::vector<std::pair<std::string, SchedulingAlgorithm *>> algorithms = {
+    std::vector<std::pair<std::string, SchedulingAlgorithm *>> algorithms = {
                 {"FCFS", new FCFS()},
                 {"SJF", new SJF()},
                 {"PRIORITY", new Priority()},
                 {"PRIORITY WITH PREEMPTION", new PriorityWithPreemption()},
-                {"ROUND ROBIN", new RoundRobin()}
-            };
-            for (auto algorithm : algorithms){
-                customCout("SIMULANDO " + algorithm.first + "\n\n", BRIGHT_YELLOW);
-                Kernel kernel(algorithm.second);
-                kernel.run();
-                customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
-            }
-            for (auto &algorithm : algorithms){
-                delete algorithm.second;
-            }
-        }
-        else if (option == 7){
-            break;
-        }
-        else{
+                {"ROUND ROBIN", new RoundRobin()}};
+    while(1){
+        customCout("INSIRA O NOME DO ALGORITMO DE ESCALONAMENTO QUE DESEJA SIMULAR\n", BRIGHT_CYAN);
+        customCout("OPCOES: 1 -> FCFS, 2 -> SJF, 3 -> PRIORITY, 4 -> PRIORITY WITH PREEMPTION,  5 -> ROUND ROBIN, 6 -> RODAR TODOS, 7 -> SAIR\n", BRIGHT_CYAN);
+        
+        int option;
+        int startInterval = 0;
+        std::cin >> option;
+        std::cout << std::endl;
+
+        if (option == 7) break;
+        if (option < 0 || 6 < option){
             customCout("OPCAO INVALIDA\n\n", BRIGHT_RED);
+            continue;
+        }
+        if (option == 6) {startInterval = 0; option = 5;}
+        else startInterval = option - 1;
+        for (int i = startInterval; i < option; i++) {
+            customCout("SIMULANDO " + algorithms[i].first + "\n\n", BRIGHT_YELLOW);
+            Kernel kernel(algorithms[i].second);
+            kernel.run();
+            customCout("FIM DA SIMULACAO\n\n", BRIGHT_YELLOW);
         }
     }
 
+    for (auto &algorithm : algorithms){
+        delete algorithm.second;
+    }
     customCout("FIM DAS SIMULACOES\n\n", BRIGHT_CYAN);
 
     return 0;
