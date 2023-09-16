207f0a45cdc923f8017d2738b0fc3a82006b5f38
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-09-16 11:17:23 -0300

    Fazendo aquela gambiarra para funcionar

--- Atividade_1/src/kernel.cpp
+++ Atividade_1/src/kernel.cpp
@@ -49,6 +49,9 @@ void Kernel::initialize() {
     cpu.setQuantum(2);
     clock = 0;
     contextSwitches = 0;
+    fixingProcessesWithTimeZero();  // Processos com tempo zero não são carregados para cpu
+                                    // São colocados como terminado e inseridos na lista de processos executados
+                                    // Desse modo, não é consumido ciclos da cpu.
 }
 
 // Método responsável pela execucao do Kernel
@@ -121,6 +124,19 @@ void Kernel::updateReadyProcesses() {
     });
     newProcesses.resize(std::distance(newProcesses.begin(), newEnd));
 
+}
+
+void Kernel::fixingProcessesWithTimeZero() {
+    std::copy_if(newProcesses.begin(), newProcesses.end(), std::back_inserter(executedProcesses), [](Process *p) {
+        int result = p->getDuration() == 0;
+        if (result) {p->setCurrentState(TERMINADO); p->setStart(0);}
+        return result;
+    });
+
+    auto newEnd = std::remove_if(newProcesses.begin(), newProcesses.end(), [](Process *p) {
+        return p->getDuration() == 0;
+    });
+    newProcesses.resize(std::distance(newProcesses.begin(), newEnd));
 }
 
 std::vector<std::vector<State>> Kernel::getExecutionHistory() const {
