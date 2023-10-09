0712165e264f8dfd965a19850b4eb09d15b8574f
Micael <mikal2003.sp@gmail.com>
2023-10-09 16:32:01 -0300

    fazendo algumas refatoracoes

--- Atividade_2/src/main.cpp
+++ Atividade_2/src/main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 #include "replacementAlgorithm.h"
-#include "analyzer.h"
+#include "simulator.h"
 
 #include <vector>
 #include <algorithm>
@@ -14,39 +14,10 @@ int main(int argc, char *argv[]) {
         std::cerr << "Usage: " << argv[0] << " <frames>" << std::endl;
         return 1;
     }
-
-    std::size_t frames = std::atoi(argv[1]);
-    std::size_t numberOfLines = 0;
-    std::size_t pfsFIFO = 0;
-    std::size_t pfsLRU = 0;
-    std::size_t pfsOPT = 0;
-
-    std::string line;
-
-    FIFOAlgorithm fifo(frames);
-    LRUAlgorithm lru(frames);
-    OPTAlgorithm opt(frames);
-
-    Analyzer analyzer;
     
-    std::vector<std::size_t> lines;
-
-    while (std::cin >> line) {
-        lines.push_back(static_cast<size_t>(std::stoi(line)));
-    }
-
-    for (std::size_t i = 0; i < lines.size(); i++){
-        std::size_t pageId = lines[i];
-        pfsFIFO += !fifo.accessMemory(pageId);
-        pfsLRU += !lru.accessMemory(pageId);
-        if (opt.full())
-            //TODO: se a lista estiver cheia e o elemento que eu quiser inserir ja esta na lista, ele nao precisaria chamar o refreshTags
-            opt.refreshTags(lines, i);
-        pfsOPT += !opt.accessMemory(pageId);
-        numberOfLines = lines.size();
-    }
-
-    analyzer.analyze(frames, numberOfLines, pfsFIFO, pfsLRU, pfsOPT);
+    std::size_t frames = std::atoi(argv[1]);
+    Simulator simulator;
+    simulator.run(frames);
 
     return 0;
 }
---
+++ Atividade_2/src/simulator.cpp
@@ -0,0 +1,41 @@
+#include "simulator.h"
+#include "replacementAlgorithm.h"
+#include <cstdio>
+#include <vector>
+
+void Simulator::run(std::size_t frames) {
+
+    std::size_t pfsFIFO = 0;
+    std::size_t pfsLRU = 0;
+    std::size_t pfsOPT = 0;
+
+    char line[256];
+
+    FIFOAlgorithm fifo(frames);
+    LRUAlgorithm lru(frames);
+    OPTAlgorithm opt(frames);
+    
+    std::vector<std::size_t> lines;
+
+    while (scanf("%s", line) != EOF) {
+        lines.push_back(static_cast<size_t>(std::stoi(line)));
+    }
+
+    for (std::size_t i = 0; i < lines.size(); i++){
+        std::size_t pageId = lines[i];
+        pfsFIFO += !fifo.accessMemory(pageId);
+        pfsLRU += !lru.accessMemory(pageId);
+        if (opt.full())
+            //TODO: se a lista estiver cheia e o elemento que eu quiser inserir ja esta na lista, ele nao precisaria chamar o refreshTags
+            opt.refreshTags(lines, i);
+        pfsOPT += !opt.accessMemory(pageId);
+    }
+
+
+    // Mudar para map o pfs?
+    printf("%lu quadros\n", frames);
+    printf("%lu refs\n", lines.size());
+    printf("FIFO: %lu PFs\n", pfsFIFO);
+    printf("LRU: %lu PFs\n", pfsLRU);
+    printf("OPT: %lu PFs\n", pfsOPT);
+}
